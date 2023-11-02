0f1cf360a3e4d92d935d0edea4f483855039059e
khushboo <aggarwal.khushi1501@gmail.com>
2023-11-02 15:08:32 +0530

    form

--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -256,9 +256,6 @@ const OrderForm: React.FC<{
               </RadioGroup>
             )}
           />
-          <p style={{ color: "red" }}>
-            {errors?.message?.intent?.fulfillment?.fulfillment_type?.message}
-          </p>
           {/* </div>
         <div
           style={{
@@ -336,6 +333,10 @@ const OrderForm: React.FC<{
             color: "red",
           }}
         >
+          <p style={{ color: "red" }}>
+            {errors?.message?.intent?.fulfillment?.fulfillment_type?.message}
+          </p>
+
           <p>
             {errors.message?.intent?.fulfillment?.start?.location?.gps?.message}
           </p>
